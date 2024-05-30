a1ddc4eac959518fe3314e01034e012dc3d1f974
Marwan Mai <emef4z@gmail.com>
2024-05-30 21:02:48 +0200

    Add custom styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">NaijaFR</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="NaijaFR" width={100} height={100} />
+            Well Connected
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse>
             <Nav className="ms-auto">
