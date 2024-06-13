2bb6f4885fda6b41c5206cc65133ff3dd7804baa
Marwan Mai <emef4z@gmail.com>
2024-06-13 20:54:30 +0200

    Add CartScreen and Add Proceed to checkout section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -21,8 +21,8 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse>
             <Nav className="ms-auto">
-              <LinkContainer to="/">
-                <Nav.Link href="/cart">
+              <LinkContainer to="/cart">
+                <Nav.Link>
                   <FaShoppingCart /> Cart
                   {cartItems.length > 0 && (
                     <Badge pill bg="success" style={{ marginLeft: "5px" }}>
@@ -31,8 +31,8 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/">
-                <Nav.Link href="/login">
+              <LinkContainer to="/login">
+                <Nav.Link>
                   <FaUser /> Sign In
                 </Nav.Link>
               </LinkContainer>
